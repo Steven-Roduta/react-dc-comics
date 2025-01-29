a52a074025a76c792221c81a6c0542bc89cb430a
Steven-Roduta <stevenroduta00@gmail.com>
2025-01-29 09:15:33 +0100

    fixed structure vite

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './controllers/Headers'
+import Main from './controllers/Main'
+import Footer from './controllers/Footer'
+import App from './App.css'
+
+export default function App() {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+  )
+}
