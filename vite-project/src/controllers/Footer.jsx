06d5362d602673a0f62a53c32b537a89fb6f6c3b
Steven-Roduta <stevenroduta00@gmail.com>
2025-01-30 18:49:16 +0100

    add main and footer list

--- vite-project/src/controllers/Footer.jsx
+++ vite-project/src/controllers/Footer.jsx
@@ -1,3 +1,143 @@
 export default function Footer() {
-    return <footer>Footer</footer>
+    return <footer>
+        <div className="list-comics">
+            <nav>
+                <ul>
+                    <li>
+                        <h3>DC COMICS</h3>
+                    </li>
+                    <li>
+                        <p>Characters</p>
+                    </li>
+                    <li>
+                        <p>Comics</p>
+                    </li>
+                    <li>
+                        <p>Movies</p>
+                    </li>
+                    <li>
+                        <p>Tv</p>
+                    </li>
+                    <li>
+                        <p>Games</p>
+                    </li>
+                    <li>
+                        <p>Videos</p>
+                    </li>
+                    <li>
+                        <p>News</p>
+                    </li>
+                    <li>
+                       <h3>SHOP</h3>
+                    </li>
+                    <li>
+                        <p>Shop DC</p>
+                    </li>
+                    <li>
+                        <p>Shop Collectables</p>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div className="list-dc">
+            <nav>
+                <ul>
+                    <li>
+                        <h4>
+                            DC
+                        </h4>
+                    </li>
+                    <li>
+                        <p>
+                            Terms of use
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                           Privacy Policy
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Add Choices
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Advertising
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Jobs
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Subscription
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Talent Workshop
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            CPCS Certificates
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Rating
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Shop Help
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Contact us
+                        </p>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div className="sites">
+            <nav>
+                <ul>
+                    <li>
+                        <h2>SITES</h2>
+                    </li>
+                    <li>
+                        <p>
+                            DC
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            MAD Megazine
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            DC Pods
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            DC Universe
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            DC Power Visa
+                        </p>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </footer>
 }
--- vite-project/src/controllers/Main.jsx
+++ vite-project/src/controllers/Main.jsx
@@ -4,5 +4,50 @@ export default function Main() {
         <div className="content">
             <h3>➡️ Content Goes Here⬅️</h3>
         </div>
+
+        <div className="dc">
+            
+            <nav>
+                <ul>
+                    <li>
+                        
+                        <div className="buy">
+                        < p>DIGITAL COMICS</p> 
+                            <img src="./img/buy-comics-digital-comics.png" alt="buy"/>
+
+                        </div>
+                      
+                    </li>
+                    <li>
+                        <div className="buy">
+                            <img src="./img/buy-comics-merchandise.png" alt="buy"/>
+                            <p>DC MECHANDICE</p>  
+                        </div>
+                       
+                    </li>
+                    <li>
+                        <div className="buy">
+                            <img src="./img/buy-comics-subscriptions.png" alt="buy"/>
+                            <p>SUBSCRIPTION</p> 
+                        </div>
+                       
+                    </li>
+                    <li>
+                        <div className="buy">
+                            <img src="./img/buy-comics-shop-locator.png" alt="buy"/>
+                            <p>COMIC SHOP LOCATOR</p>  
+                        </div>
+                       
+                    </li>
+                    <li>
+                        <div className="buy">
+                            <img src="./img/buy-dc-power-visa.svg" alt="buy"/>
+                            <p>DC POWER VISA</p> 
+                        </div>
+                      
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </main>
 }
